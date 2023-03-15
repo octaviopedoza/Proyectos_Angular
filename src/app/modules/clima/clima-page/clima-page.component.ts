851f190d6c2e7f53eebd553ff9753e4459205238
octaviopedoza <octavio.pedroza.bn@gmail.com>
2023-03-15 14:54:12 -0600

    Se consume api del clima y se sacan datos

--- src/app/modules/clima/clima-page/clima-page.component.ts
+++ src/app/modules/clima/clima-page/clima-page.component.ts
@@ -7,33 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clima-page.component.css']
 })
 export class ClimaPageComponent implements OnInit{
+  cargando:boolean = false;
   ipVisitante:string = '';
   longitud:string = '';
   latitud:string = '';
+  state:string='';
+  city:string='';
+  date:string='';
+  anio:string='';
+  mes:string='';
+  dia:string='';
+
+
   datos:Array<any>=[];
 
   constructor(private climaService:ClimaService){ }
 
   ngOnInit(): void {
+    this.cargando = true;
     this.getlocation();
     this.getclima();
+    this.cargando = false;
   }
 
   getlocation(): void{
     this.climaService.getLocation().subscribe((data:any) =>{
       this.latitud = data.latitude;
       this.longitud = data.longitude;
-      console.log('latitud: ',this.latitud)
-      console.log('longitud: ',this.longitud)
+      this.state = data.region_name;
+      this.city = data.city;
     })
   }
 
   getclima(): void{
     this.climaService.getClima(this.longitud,this.latitud).subscribe((data:any) =>{
       this.datos = data.dataseries;
-      console.log('datos del clima: ',data)
-      console.log('datos del clima: ',this.datos)
+      this.date = this.datos[0].date.toString();
+      this.anio = this.date.slice(0,4)
+      this.mes = this.date.slice(4,6)
+      this.dia = this.date.slice(6,8)
     })
+      
   }
 
 }
--- src/app/modules/clima/clima.module.ts
+++ src/app/modules/clima/clima.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClimaPageComponent } from './clima-page/clima-page.component';
-
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
@@ -9,7 +9,8 @@ import { ClimaPageComponent } from './clima-page/clima-page.component';
     ClimaPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
   ]
 })
 export class ClimaModule { }
